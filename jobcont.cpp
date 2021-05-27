96360951a23173595b5080aa9c2d8d17982a1a77
nawat <67263644+justnawat@users.noreply.github.com>
2021-05-28 01:53:22 +0700

    starting on milestone 6 as well

---
+++ jobcont.cpp
@@ -0,0 +1,34 @@
+// checks if the command has to be run in the background or not
+bool checkbg(string commandLine) {
+    if (commandLine[commandLine.length() - 1] == '&') {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+// just prints out the current jobs
+void myjob() {
+    if (head == NULL) { // no current job
+        return;
+    } else { // has jobs
+        current = head; // start at the head
+    }
+
+    while (current != NULL) { // print each job
+        cout << "[" << current->jid << "]\t"; // job id
+        cout << current->age << "\t"; // blank, -, or +
+        cout << current->stat << "\t"; // status
+        cout << current->cmd; // the command itself
+        current = current->next; // goes to the next node
+    }
+    return;
+}
+
+void make_foreground(string commandLine) {
+    // do something
+}
+
+void make_background(string commandLine) {
+    // do something
+}
